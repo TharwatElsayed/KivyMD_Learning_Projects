eb53235712b35f875a0a47e14852c1b15a00a26b
Eng.\ Tharwat El-Sayed <tharwat_uss89@hotmail.com>
2025-10-29 22:22:37 +0300

    Add files via upload

---
+++ Switch between Screens/main.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+class HomeScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    pass
+
+GUI = Builder.load_file("main.kv")
+
+class MainApp(App):
+    def build(self):
+        return GUI
+    
+    def change_screen(self,screen_name):
+        screen_manager = self.root.ids['screen_manager']
+        screen_manager.transition
+        screen_manager.current = screen_name
+
+MainApp().run()
